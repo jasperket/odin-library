316d5a9abbf4b5038d1e9bf68a67c6da65615fbf
jasperket <jasperkeith1.vallecera@gmail.com>
2023-10-02 19:53:23 +0800

    Finished adding books to library functionality

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ function displayLibrary() {
 
         let read = ""
 
-        if(book.read) {
+        if(myLibrary[i].read) {
             read = "Read"
         } else {
             read = "Unread"
         }
 
-        card.innerHTML = "<h2>"+book.title+"</h2>"+
-        "<h3>"+book.author+"</h3>"+
-        "<p>Pages: "+book.pages+"</p>"+
+        card.innerHTML = "<h2>"+myLibrary[i].title+"</h2>"+
+        "<h3>"+myLibrary[i].author+"</h3>"+
+        "<p>Pages: "+myLibrary[i].pages+"</p>"+
         "<button class='"+read.toLowerCase()+"'>"+read+"</button>"+
         "<button class='remove' data-index='"+i+"'>Remove</button>";
 
@@ -44,7 +44,25 @@ function displayLibrary() {
     }
 }
 
-btnSubmitBook = document.querySelector('submit-book');
-btnSubmitBook.addEventListener('submit',() => {
+const btnClose = document.querySelector('.close-dialog');
+btnClose.addEventListener('click',() => {
+    addDialog.close();
+})
+
+const btnSubmitBook = document.querySelector('.submit-book');
+btnSubmitBook.addEventListener('click',(e) => {
+    e.preventDefault();
     
+    const title = document.querySelector('#add-title').value;
+    const author = document.querySelector('#add-author').value;
+    const pages = parseInt(document.querySelector('#add-pages').value);
+    const read = document.querySelector('#add-read').checked;
+
+    const book = new Book(title,author,pages,read);
+    addBookToLibrary(book);
+    addDialog.close();
+    displayLibrary();
+
+    const form = document.querySelector('#add-book');
+    form.reset();
 })
