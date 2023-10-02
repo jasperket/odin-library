99e97cb06959664e3b829d7249cb9c43354c47dd
jasperket <jasperkeith1.vallecera@gmail.com>
2023-10-02 19:20:19 +0800

    Added styles to the add book dialog

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const myLibrary = [];
 
+const addDialog = document.querySelector('dialog');
+const btnAdd = document.querySelector('.add');
+
+btnAdd.addEventListener('click', () => {
+    addDialog.showModal();
+})
+
 function Book(title,author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,7 +21,8 @@ function addBookToLibrary(book) {
 function displayLibrary() {
     const library = document.querySelector('.library');
     library.innerHTML = "";
-    myLibrary.forEach((book) => {
+
+    for(let i = 0; i < myLibrary.length; i++) {
         const card = document.createElement('div');
         card.classList.add('card');
 
@@ -30,8 +38,13 @@ function displayLibrary() {
         "<h3>"+book.author+"</h3>"+
         "<p>Pages: "+book.pages+"</p>"+
         "<button class='"+read.toLowerCase()+"'>"+read+"</button>"+
-        "<button class='remove'>Remove</button>";
+        "<button class='remove' data-index='"+i+"'>Remove</button>";
 
         library.appendChild(card);
-    })
+    }
 }
+
+btnSubmitBook = document.querySelector('submit-book');
+btnSubmitBook.addEventListener('submit',() => {
+    
+})
